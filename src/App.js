72f63548d8af2a5e9d38f73fc73abc9f255d0b9b
makadown <maseflores79@gmail.com>
2019-04-20 17:49:44 -0500

    Proyecto reconstruido de cero

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <p id="someId">This is JSX from app.js!</p>
+    );
+  }
+}
+
+export default App;
